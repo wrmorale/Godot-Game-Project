fd2e9b89a77bbf56ce19e6c468ea76f39a40c196
Wrmorales <wrmorale@ucsc.edu>
2024-06-10 00:05:25 -0700

    Bullet shooting Mechanic

---
+++ Scripts/bullet.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class bullet : RigidBody2D
+{
+	public override void _Ready(){
+		Timer timer = GetNode<Timer>("Timer");
+		timer.Timeout += () => QueueFree();
+	}
+}
